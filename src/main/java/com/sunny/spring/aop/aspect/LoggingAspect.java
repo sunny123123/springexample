5aec315a3894fea8c3622f2af9a8e62073a73d42
sunny <sunny4handsome@163.com>
2016-04-06 21:34:15 +0800

    1.spring aop aspectj annotation configuration

--- src/main/java/com/sunny/spring/aop/aspect/LoggingAspect.java
+++ src/main/java/com/sunny/spring/aop/aspect/LoggingAspect.java
@@ -1,6 +1,13 @@
 package com.sunny.spring.aop.aspect;
 
+import java.util.Arrays;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -12,4 +19,47 @@ public class LoggingAspect {
 		System.out.println("hijacked : " + joinPoint.getSignature().getName());
 		System.out.println("******");
 	}
+	@After("execution(* com.sunny.spring.aop.service.Customer.addCustomer(..))")
+	public void logAfter(JoinPoint joinPoint) {
+
+		System.out.println("logAfter() is running!");
+		System.out.println("hijacked : " + joinPoint.getSignature().getName());
+		System.out.println("******");
+	}
+	@AfterReturning(
+		      pointcut = "execution(* com.sunny.spring.aop.service.Customer.addCustomerReturnValue(..))",
+		      returning= "result")
+		   public void logAfterReturning(JoinPoint joinPoint, Object result) {
+
+			System.out.println("logAfterReturning() is running!");
+			System.out.println("hijacked : " + joinPoint.getSignature().getName());
+			System.out.println("Method returned value is : " + result);
+			System.out.println("******");
+
+		   }
+	  @AfterThrowing(
+		      pointcut = "execution(* com.sunny.spring.aop.service.Customer.addCustomerThrowException(..))",
+		      throwing= "error")
+		    public void logAfterThrowing(JoinPoint joinPoint, Throwable error) {
+
+			System.out.println("logAfterThrowing() is running!");
+			System.out.println("hijacked : " + joinPoint.getSignature().getName());
+			System.out.println("Exception : " + error);
+			System.out.println("******");
+		    }
+	  
+	  @Around("execution(* com.sunny.spring.aop.service.Customer.addCustomerAround(..))")
+	   public void logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		System.out.println("logAround() is running!");
+		System.out.println("hijacked method : " + joinPoint.getSignature().getName());
+		System.out.println("hijacked arguments : " + Arrays.toString(joinPoint.getArgs()));
+			
+		System.out.println("Around before is running!");
+		joinPoint.proceed(); //continue on the intercepted method
+		System.out.println("Around after is running!");
+			
+		System.out.println("******");
+
+	   }
 }
--- src/main/java/com/sunny/spring/aop/test/Test.java
+++ src/main/java/com/sunny/spring/aop/test/Test.java
@@ -5,12 +5,28 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.sunny.spring.aop.service.Customer;
 
+/*
+ * Common AspectJ annotations :
+
+	@Before – Run before the method execution
+	@After – Run after the method returned a result
+	@AfterReturning – Run after the method returned a result, intercept the returned result as well.
+	@AfterThrowing – Run after the method throws an exception
+	@Around – Run around the method execution, combine all three advices above.
+ */
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("Spring-Customer.xml");
         Customer customer = (Customer)appContext.getBean("customer");
-        customer.addCustomer();
+        //customer.addCustomer();
+        //customer.addCustomerReturnValue();
+      /*  try {
+			customer.addCustomerThrowException();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}*/
+        customer.addCustomerAround("sunny");
 	}
 
 }
